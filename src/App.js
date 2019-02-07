b7bf9b2400c3c95521b65f415a54a3c2f0d92b08
aleclivinghouse <aleclivinghouse@yahoo.com>
2019-02-07 15:55:18 -0800

    second commit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import './App.css';
 import  {Provider} from 'react-redux';
 import Posts from './components/Posts';
 import PostForm from './components/PostForm';
-import {createStore, applyMiddleware} from 'redux';
+import store from './store';
 
-
-const store = createStore(() => [], {}, applyMiddleware());
+// const store = createStore(() => [], {}, applyMiddleware());
 
 class App extends Component {
   render() {
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,33 +1,38 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/postActions';
 
-class Posts extends Component{
-  constructor(props){
-    super();
-    this.state = {
-      posts: []
+class Posts extends Component {
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.newPost) {
+      this.props.posts.unshift(nextProps.newPost);
     }
   }
-  componentWillMount(){
-    console.log('component mounted');
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(res => res.json())
-    .then(data => this.setState({posts: data}));
-  }
+
   render() {
-    const postItems = this.state.posts.map(post => (
+    const postItems = this.props.posts.map(post => (
       <div key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.body}</p>
       </div>
     ));
-
-    return(
+    return (
       <div>
-        <h1>posts</h1>
+        <h1>Posts</h1>
         {postItems}
       </div>
     );
   }
 }
 
-export default Posts;
+const mapStateToProps = state => ({
+  posts: state.posts.items,
+  newPost: state.posts.item
+});
+
+export default connect(mapStateToProps, { fetchPosts })(Posts);
